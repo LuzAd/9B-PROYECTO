322aadea03e33f49083581fb25eba746de196035
LuzAd <lreyesgonzalez94@gmail.com>
2024-07-19 17:33:35 -0600

    Cambios bd

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_cVz01GlqxebRpiegDH-@mysql-a6f30af-utxicotepec-b73d.l.aivencloud.com:24948/defaultdb"
-#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/test"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_-6qDMubGmO3jCgSwQW-@mysql-3b6a263a-apigymdec6.l.aivencloud.com:15406/defaultdb"
+#"mysql+pymysql://root:1234@localhost:3306/test"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base=declarative_base()
