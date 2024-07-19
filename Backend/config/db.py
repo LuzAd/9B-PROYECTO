9444d1c01a92b862ed70cb8ca219555c486599b9
LuzAd <lreyesgonzalez94@gmail.com>
2024-07-19 15:59:25 -0600

    CAmbios

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/test"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_-6qDMubGmO3jCgSwQW-@mysql-3b6a263a-apigymdec6.l.aivencloud.com:15406/defaultdb"
+#"mysql+pymysql://root:1234@localhost:3307/test"
 #"mysql+pymysql://avnadmin:AVNS_-6qDMubGmO3jCgSwQW-@mysql-3b6a263a-apigymdec6.l.aivencloud.com:15406/defaultdb"
 
 #"mysql+pymysql://root:1234@localhost:3306/test"
